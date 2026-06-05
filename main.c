3cadc1c5f96099dc84029cd0bfdb15ec5be9d01b
thanaya412-svg <thanaya412@gmail.com>
2026-06-05 09:05:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+#define WIDTH 80
+#define HEIGHT 24
+#define EMPTY '_'
+
+char picture[HEIGHT][WIDTH];
+
+int main()
+{
+    printf("Canvas created successfully!\n");
+    printf("Canvas size: %d x %d\n", WIDTH, HEIGHT);
+
+    return 0;
+}
