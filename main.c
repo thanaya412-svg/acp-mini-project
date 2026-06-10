82d78959bce41f11599665fcace5967fd1c9d563
thanaya412-svg <thanaya412@gmail.com>
2026-06-10 09:47:25 +0530

    Update main.c
    date 10-6-2026
    day wednesday

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #define WIDTH 80
 #define HEIGHT 24
 #define EMPTY '_'
+#define PIXEL'*'
+
 
 char picture[HEIGHT][WIDTH];
 void clearPicture()
@@ -29,7 +31,13 @@ void displayPicture()
         printf("\n");
     }
 }
-
+void setPixel(int x, int y)
+{
+    if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+    {
+        picture[y][x] = PIXEL;
+    }
+}
 void drawLine(int x1, int y1, int x2, int y2)
 {
     int dx = abs(x2 - x1);
@@ -75,10 +83,101 @@ void drawRectangle(int x1,int y1,int x2,int y2)
         setPixel(x2, y);
     }
 }
+void drawCircle(int cx, int cy, int r)
+{
+    int x = r;
+    int y = 0;
+    int err = 0;
+
+    while (x >= y)
+    {
+        setPixel(cx + x, cy + y);
+        setPixel(cx + y, cy + x);
+        setPixel(cx - y, cy + x);
+        setPixel(cx - x, cy + y);
+        setPixel(cx - x, cy - y);
+        setPixel(cx - y, cy - x);
+        setPixel(cx + y, cy - x);
+        setPixel(cx + x, cy - y);
+
+        y++;
+
+        if (err <= 0)
+            err += 2 * y + 1;
+        else
+        {
+            x--;
+            err -= 2 * x + 1;
+        }
+    }
+}
+void drawTriangle(int x1, int y1,
+                  int x2, int y2,
+                  int x3, int y3)
+{
+    drawLine(x1, y1, x2, y2);
+    drawLine(x2, y2, x3, y3);
+    drawLine(x3, y3, x1, y1);
+}
 int main()
 {
-    
-     printf("Canvas created successfully!\n");
-     printf("Canvas size: %d x %d\n", WIDTH, HEIGHT);
-     return 0;
+   int choice;
+
+    clearPicture();
+
+    while (1)
+    {
+    printf("\n===== ASCII Drawing Program =====\n");
+    printf("1. Draw Line\n");
+     printf("2. Draw Rectangle\n");
+     printf("3. Draw Circle\n");
+     printf("4. Draw Triangle\n");
+     printf("5. Display Canvas\n");
+     printf("6. Clear Canvas\n");
+     printf("0. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            drawLine(5, 5, 25, 15);
+            printf("Line drawn.\n");
+            break;
+
+        case 2:
+            drawRectangle(35, 5, 55, 15);
+            printf("Rectangle drawn.\n");
+            break;
+
+        case 3:
+            drawCircle(20, 10, 5);
+            printf("Circle drawn.\n");
+            break;
+
+        case 4:
+            drawTriangle(60, 5, 70, 15, 50, 15);
+            printf("Triangle drawn.\n");
+            break;
+
+        case 5:
+            displayPicture();
+            break;
+
+        case 6:
+            clearPicture();
+            printf("Canvas cleared.\n");
+            break;
+
+        case 0:
+            printf("Exiting program.\n");
+            return 0;
+
+        default:
+            printf("Invalid choice.\n");
+        }
+    }
+
+    return 0; 
+  
 }
